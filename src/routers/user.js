3f0deb9930d2e8311c59d9c25a370596954e892b
Pyaesone Khant <pyaepsk@gmail.com>
2024-08-05 13:42:45 +0630

    feat: develop follow endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,15 +7,26 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     try {
-        const data = await prisma.user.findMany();
+        const data = await prisma.user.findMany({
+            include: {
+                posts: true,
+                comments: true,
+                followers: true,
+                following: true
+            },
+            orderBy: {
+                id: "desc"
+            },
+            take: 20
+        });
         res.json(data);
     } catch (error) {
         res.status(500).json({ error })
     }
 })
 
-router.get("/verify", auth, async(req, res) => {
-    const {user} = res.locals;
+router.get("/verify", auth, async (req, res) => {
+    const { user } = res.locals;
     res.json(user);
 })
 
@@ -27,14 +38,19 @@ router.get("/:id", async (req, res) => {
                 id: Number(id)
             },
             select: {
+                id: true,
                 name: true,
                 username: true,
                 bio: true,
                 posts: {
                     include: {
-                        user: true
+                        user: true,
+                        likes: true,
+                        comments: true
                     }
-                }
+                },
+                followers: true,
+                following: true
             },
         });
         res.json(data);
@@ -64,6 +80,34 @@ router.post("/", async (req, res) => {
     } catch (error) {
         res.status(500).json({ error })
     }
+})
+
+router.post("/:id/follow", auth, async (req, res) => {
+    const { user } = res.locals
+    const { id } = req.params;
+
+    const data = await prisma.follow.create({
+        data: {
+            followerId: Number(user.id),
+            followingId: Number(id)
+        }
+    })
+
+    res.json(data)
+});
+
+router.delete("/:id/unfollow", auth, async (req, res) => {
+    const { user } = res.locals
+    const { id } = req.params;
+
+    await prisma.follow.deleteMany({
+        where: {
+            followerId: Number(user.id),
+            followingId: Number(id)
+        }
+    })
+
+    res.json({ message: `Unfollowed user ${id}` });
 })
 
 module.exports = { UserRouter: router }
